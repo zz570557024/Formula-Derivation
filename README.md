99754c2b72c70a20edf2e893995cb5952312676b
RuShi <570557024@qq.com>
2021-07-26 15:51:59 +0800

    init add
    init add

--- README.md
+++ README.md
@@ -1,20 +1,25 @@
-# FormulaDerivation
-微信小程序计算器
-=======
-# FormulaDerivation(试飞参数计算器)
 
-> 技术框架
+# FormulaDerivation--试飞参数计算器
+
+## 技术框架
 * [微信小程序原生语言](https://developers.weixin.qq.com/miniprogram/dev/framework/)
 
-> UI框架
+## UI框架
 * [WeUI](https://weui.io/)
 
-> 包含以下计算类型：
+## 实现功能
 * 表速、真速、马赫数计算
 * 动压、静压计算
 * 标准大气物理量计算
 * 长度单位计算
 * 速度单位计算
 
-> 作者：RuShi
-> MIT 2021
+## 截图
+![p1](./image/jt1.jpg)
+![p2](./image/jt2.jpg)
+
+## Contribution
+RuShi
+
+## License
+MIT
---
+++ utils/atm.js
@@ -0,0 +1,281 @@
+const atm = [{
+    "height": -0.5,
+    "temperature": 291.4,
+    "pressure": 107477,
+    "density": 1.285
+  },
+  {
+    "height": 0,
+    "temperature": 288.1,
+    "pressure": 101325,
+    "density": 1.225
+  },
+  {
+    "height": 0.5,
+    "temperature": 284.9,
+    "pressure": 95461,
+    "density": 1.167
+  },
+  {
+    "height": 1,
+    "temperature": 281.7,
+    "pressure": 89876,
+    "density": 1.112
+  },
+  {
+    "height": 1.5,
+    "temperature": 278.4,
+    "pressure": 84559,
+    "density": 1.058
+  },
+  {
+    "height": 2,
+    "temperature": 275.2,
+    "pressure": 79501,
+    "density": 1.007
+  },
+  {
+    "height": 2.5,
+    "temperature": 271.9,
+    "pressure": 74691,
+    "density": 0.957
+  },
+  {
+    "height": 3,
+    "temperature": 268.7,
+    "pressure": 70121,
+    "density": 0.909
+  },
+  {
+    "height": 3.5,
+    "temperature": 265.4,
+    "pressure": 65780,
+    "density": 0.863
+  },
+  {
+    "height": 4,
+    "temperature": 262.2,
+    "pressure": 61660,
+    "density": 0.819
+  },
+  {
+    "height": 4.5,
+    "temperature": 258.9,
+    "pressure": 57752,
+    "density": 0.777
+  },
+  {
+    "height": 5,
+    "temperature": 255.7,
+    "pressure": 54048,
+    "density": 0.736
+  },
+  {
+    "height": 5.5,
+    "temperature": 252.4,
+    "pressure": 50539,
+    "density": 0.697
+  },
+  {
+    "height": 6,
+    "temperature": 249.2,
+    "pressure": 47217,
+    "density": 0.66
+  },
+  {
+    "height": 6.5,
+    "temperature": 245.9,
+    "pressure": 44075,
+    "density": 0.624
+  },
+  {
+    "height": 7,
+    "temperature": 242.7,
+    "pressure": 41105,
+    "density": 0.59
+  },
+  {
+    "height": 7.5,
+    "temperature": 239.5,
+    "pressure": 38299,
+    "density": 0.557
+  },
+  {
+    "height": 8,
+    "temperature": 236.2,
+    "pressure": 35651,
+    "density": 0.526
+  },
+  {
+    "height": 8.5,
+    "temperature": 233,
+    "pressure": 33154,
+    "density": 0.496
+  },
+  {
+    "height": 9,
+    "temperature": 229.7,
+    "pressure": 30800,
+    "density": 0.467
+  },
+  {
+    "height": 9.5,
+    "temperature": 226.5,
+    "pressure": 28584,
+    "density": 0.44
+  },
+  {
+    "height": 10,
+    "temperature": 223.3,
+    "pressure": 26499,
+    "density": 0.414
+  },
+  {
+    "height": 10.5,
+    "temperature": 220,
+    "pressure": 24540,
+    "density": 0.389
+  },
+  {
+    "height": 11,
+    "temperature": 216.8,
+    "pressure": 22699,
+    "density": 0.365
+  },
+  {
+    "height": 11.5,
+    "temperature": 216.6,
+    "pressure": 20984,
+    "density": 0.337
+  },
+  {
+    "height": 12,
+    "temperature": 216.6,
+    "pressure": 19399,
+    "density": 0.312
+  },
+  {
+    "height": 13,
+    "temperature": 216.6,
+    "pressure": 16580,
+    "density": 0.267
+  },
+  {
+    "height": 14,
+    "temperature": 216.6,
+    "pressure": 14170,
+    "density": 0.228
+  },
+  {
+    "height": 15,
+    "temperature": 216.6,
+    "pressure": 12112,
+    "density": 0.195
+  },
+  {
+    "height": 16,
+    "temperature": 216.6,
+    "pressure": 10353,
+    "density": 0.167
+  },
+  {
+    "height": 17,
+    "temperature": 216.6,
+    "pressure": 8849.7,
+    "density": 0.432
+  },
+  {
+    "height": 18,
+    "temperature": 216.6,
+    "pressure": 7565.2,
+    "density": 0.1217
+  },
+  {
+    "height": 19,
+    "temperature": 216.6,
+    "pressure": 6467.5,
+    "density": 0.1040
+  },
+  {
+    "height": 20,
+    "temperature": 216.6,
+    "pressure": 5529.3,
+    "density": 0.08891
+  },
+  {
+    "height": 25,
+    "temperature": 221.5,
+    "pressure": 2549.2,
+    "density": 0.04008
+  },
+  {
+    "height": 30,
+    "temperature": 226.5,
+    "pressure": 1197.0,
+    "density": 0.01841
+  },
+  {
+    "height": 35,
+    "temperature": 236.5,
+    "pressure": 574.6,
+    "density": 0.008463
+  },
+  {
+    "height": 40,
+    "temperature": 250.4,
+    "pressure": 287.14,
+    "density": 0.003996
+  },
+  {
+    "height": 45,
+    "temperature": 264.2,
+    "pressure": 149.1,
+    "density": 0.001966
+  },
+  {
+    "height": 50,
+    "temperature": 270.7,
+    "pressure": 79.779,
+    "density": 0.001024
+  },
+  {
+    "height": 55,
+    "temperature": 265.6,
+    "pressure": 42.7516,
+    "density": 0.0005608
+  },
+  {
+    "height": 60,
+    "temperature": 255.8,
+    "pressure": 22.461,
+    "density": 0.0003059
+  },
+  {
+    "height": 65,
+    "temperature": 239.3,
+    "pressure": 11.446,
+    "density": 0.0001667
+  },
+  {
+    "height": 70,
+    "temperature": 219.7,
+    "pressure": 5.5205,
+    "density": 0.0000875
+  },
+  {
+    "height": 75,
+    "temperature": 200.2,
+    "pressure": 2.4904,
+    "density": 0.0000434
+  },
+  {
+    "height": 80,
+    "temperature": 180.7,
+    "pressure": 1.0366,
+    "density": 0.00002
+  }
+]
+
+module.exports = {
+  atm: atm
+}
